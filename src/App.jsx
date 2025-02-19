9a1913c3f3a79ac0155e80bf43771d72e872ca1d
Kosdevua <konstantin.vodolozhko@gmail.com>
2025-02-20 00:39:33 +0200

    added Balance

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
+function Balance({ entries }) {
+  const balance = entries.reduce(
+    (acc, entry) => acc + parseFloat(entry.amount),
+    0
+  );
+  return (
+    <div className="text-center text-xl font-bold mt-4">
+      Баланс: {balance} грн
+    </div>
+  );
+}
+
 function App() {
   const [entries, setEntries] = useState(() => {
     const savedEntries = localStorage.getItem("entries");
     return savedEntries ? JSON.parse(savedEntries) : [];
   });
+  // const [count, setCount] = useState("");
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
   const [date, setDate] = useState("");
@@ -37,6 +50,7 @@ function App() {
 
   const columns = React.useMemo(
     () => [
+      { Header: "№", accessor: (_, index) => index + 1 },
       { Header: "Дата", accessor: "date" },
       { Header: "Категорія", accessor: "category" },
       {
@@ -95,99 +109,102 @@ function App() {
       return;
     }
 
-    setEntries([
-      ...entries,
-      {
-        date,
-        category,
-        amount: `${type === "income" ? "+" : "-"}${amount}`,
-        type,
-      },
-    ]);
+    const newEntry = {
+      date,
+      category,
+      amount: `${type === "income" ? "+" : "-"}${amount}`,
+      type,
+    };
+
+    setEntries([...entries, newEntry]);
     setAmount("");
     setCategory("");
     setDate("");
+    toast.success("Транзакцію додано успішно!");
   };
 
   return (
     <div className="container mx-auto p-4 max-w-lg">
+      <div className="max-w-[320px] mx-auto"></div>
       <h1 className="text-3xl text-center mt-18 mb-4">Фінансовий трекер</h1>
       <ToastContainer />
 
-      <div className="flex flex-col gap-3">
-        <input
-          type="date"
-          className="border p-2 w-full rounded-md"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
+      <div className="w-100">
+        <div className="flex flex-col gap-3">
+          <input
+            type="date"
+            className="border p-2 w-full rounded-md"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
 
-        <select
-          className="border p-2 w-full rounded-md"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="">Оберіть категорію</option>
-          {categories.map((cat, index) => (
-            <option key={index} value={cat}>
-              {cat}
-            </option>
-          ))}
-        </select>
+          <select
+            className="border p-2 w-full rounded-md"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">Оберіть категорію</option>
+            {categories.map((cat, index) => (
+              <option key={index} value={cat}>
+                {cat}
+              </option>
+            ))}
+          </select>
 
-        <div className="flex w-full">
+          <div className="flex w-full">
+            <button
+              className={`p-2 w-1/2 rounded-l border ${
+                type === "expense"
+                  ? "bg-gray-700  text-zinc-300 border-0"
+                  : "bg-gray-300 text-black"
+              }`}
+              onClick={() => setType("expense")}
+            >
+              Витрати
+            </button>
+            <button
+              className={`p-2 w-1/2 rounded-r border ${
+                type === "income"
+                  ? "bg-gray-700 text-zinc-300 border-0"
+                  : "bg-gray-300 text-black"
+              }`}
+              onClick={() => setType("income")}
+            >
+              Дохід
+            </button>
+          </div>
+
+          <input
+            type="text"
+            inputMode="numeric"
+            pattern="[0-9]*"
+            className="border p-2 w-full rounded-md appearance-none"
+            placeholder="Сума"
+            value={amount}
+            onChange={(e) => {
+              if (e.target.value.match(/^\d*$/)) {
+                setAmount(e.target.value);
+              }
+            }}
+          />
+
           <button
-            className={`p-2 w-1/2 rounded-l border ${
-              type === "income"
-                ? "bg-gray-700 text-zinc-300 border-0"
-                : "bg-gray-300 text-black"
-            }`}
-            onClick={() => setType("income")}
+            onClick={handleAddEntry}
+            className="w-full bg-gray-700 text-zinc-300 p-4  rounded-xs "
           >
-            Дохід
-          </button>
-          <button
-            className={`p-2 w-1/2 rounded-r border ${
-              type === "expense"
-                ? "bg-gray-700  text-zinc-300 border-0"
-                : "bg-gray-300 text-black"
-            }`}
-            onClick={() => setType("expense")}
-          >
-            Витрати
+            Додати
           </button>
         </div>
-
-        <input
-          type="text"
-          inputMode="numeric"
-          pattern="[0-9]*"
-          className="border p-2 w-full rounded-md appearance-none"
-          placeholder="Сума"
-          value={amount}
-          onChange={(e) => {
-            if (e.target.value.match(/^\d*$/)) {
-              setAmount(e.target.value);
-            }
-          }}
-        />
-
-        <button
-          onClick={handleAddEntry}
-          className="w-full bg-gray-700 text-zinc-300 p-4  rounded-xs "
-        >
-          Додати
-        </button>
+        <Balance entries={entries} />
       </div>
-
       <div className="flex gap-2 my-4">
         <select
           className="p-2 border rounded-md"
           onChange={(e) => setFilterType(e.target.value)}
         >
           <option value="all">Тив витрат</option>
-          <option value="income">Дохід</option>
           <option value="expense">Витрати</option>
+          <option value="income">Дохід</option>
         </select>
 
         <select
@@ -198,9 +215,46 @@ function App() {
           <option value="amount">Сортувати за сумою</option>
         </select>
       </div>
-      <div>
-        <div className="max-w-miş"></div>
-      </div>
+
+      <table
+        {...getTableProps()}
+        className="min-w-full table-auto border-collapse border border-gray-300 mt-4"
+      >
+        <thead>
+          {headerGroups.map((headerGroup, index) => (
+            <tr {...headerGroup.getHeaderGroupProps()} key={index}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  key={column.id}
+                  className="border p-2 bg-gray-200"
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, index) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} key={index}>
+                {row.cells.map((cell) => (
+                  <td
+                    {...cell.getCellProps()}
+                    className="border p-2 text-right"
+                    key={cell.column.id}
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
