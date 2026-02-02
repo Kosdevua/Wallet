e096efdbe33dc744fa831584b7994d65ff99c054
Kosdevua <konstantin.vodolozhko@gmail.com>
2026-02-02 22:13:38 +0200

    Update Balance.jsx

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,8 +1,8 @@
 function Balance({ entries }) {
-  const balance = entries.reduce(
-    (acc, entry) => acc + parseFloat(entry.amount),
-    0
-  );
+  const balance = entries.reduce((acc, entry) => {
+    const amount = parseFloat(entry.amount) || 0;
+    return entry.type === "income" ? acc + amount : acc - amount;
+  }, 0);
   return (
     <div className="text-center text-2xl font-bold mt-4 border-1">
       Баланс: {balance} грн
